faf1d723be2cb864ee7988564dd58e236c31325e
hui <hui02222@naver.com>
2021-01-09 23:54:52 +0900

    jpa get detail

--- src/main/java/com/example/jpa/controller/MainController.java
+++ src/main/java/com/example/jpa/controller/MainController.java
@@ -1,11 +1,10 @@
 package com.example.jpa.controller;
 
 import com.example.jpa.domain.Product;
+import com.example.jpa.repository.ProductRepository;
 import com.example.jpa.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,6 +13,9 @@ public class MainController {
 
     @Autowired
     ProductService productService;
+
+    @Autowired
+    ProductRepository productRepository;
 
 
     @RequestMapping(path = "/product/list",method = RequestMethod.GET)
@@ -24,6 +26,15 @@ public class MainController {
         return productList;
     }
 
+    @RequestMapping(path = "/product/detail/{productId}",method = RequestMethod.GET)
+    public Product getProductOne(
+            @PathVariable int productId
+    ) {
+
+        Product productOne = productRepository.findByProductId(productId);
+
+        return productOne;
+    }
 
 
 
--- src/main/java/com/example/jpa/repository/ProductRepository.java
+++ src/main/java/com/example/jpa/repository/ProductRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ProductRepository extends JpaRepository<Product,Integer> {
 
     Product findAllByProductId(String id);
+    Product findByProductId(int productId);
     Product findByProductCode(String productCode);
 }
