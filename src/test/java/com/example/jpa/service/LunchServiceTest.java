4713dd0246f11501f322a0a3cc3c8836e18a6984
hui <hui02222@naver.com>
2021-09-16 22:26:15 +0900

    랜덤 이심점심조 구현

--- src/test/java/com/example/jpa/service/LunchServiceTest.java
+++ src/test/java/com/example/jpa/service/LunchServiceTest.java
@@ -3,6 +3,7 @@ package com.example.jpa.service;
 import com.example.jpa.GroupCalculator;
 import com.example.jpa.domain.Member;
 import com.example.jpa.repository.MemberRepository;
+import com.example.jpa.util.TeamEnum;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,12 +12,8 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
@@ -45,52 +42,82 @@ class LunchServiceTest {
         groupCal = new GroupCalculator();
     }
 
+
     @Test
-    public void test() {
+    public void teamShuffle() {
+        Map<String, List<Member>> teamInfo = new HashMap<>();
+        Long[] members = {1L,2L,3L,4L,5L,6L,7L,8L,9L,10L,12L,13L,14L,15L,16L};
+
+        // 팀별 리스트 Map 에 넣기
+        Arrays.stream(TeamEnum.values()).forEach( x -> {
+            List<Member> memberList = memberRepository.findAllByMemberIdInAndTeam(members, x.getTeamName());
+            Collections.shuffle(memberList);
+            teamInfo.put(x.getTeam(), memberList);
+        });
+
+        teamInfo.forEach((groupIndex,memberList)->{
+            System.out.println(groupIndex);
+            memberList.forEach(member->{
+                System.out.println(member.getMemberName());
+            });
+        });
+
+    }
+
+    @Test
+    public void randomLunchTeam() {
         // todo 1. 인원을 받음
+        Map<String, List<Member>> teamInfo = new HashMap<>();
         Map<String, List<Member>> group = new HashMap<>();
 
-        List<Member> memberList = memberRepository.findAll(); // todo 정렬필요
-        int n = memberList.size();
+        Long[] members = {1L,2L,3L,4L,5L,6L,7L,8L,9L,10L,12L,13L,14L,15L,16L};
+        int n = members.length;
 
+        // 팀별 리스트 Map 에 넣기
+        Arrays.stream(TeamEnum.values()).forEach( x -> {
+            // 팀별 리스트
+            List<Member> memberList = memberRepository.findAllByMemberIdInAndTeam(members, x.getTeamName());
+            // 팀별로 중복이 없도록 처리하는 로직 추가
+            Collections.shuffle(memberList);
+            teamInfo.put(x.getTeam(), memberList);
+        });
 
         // todo 2. 인원 수로 그룹4, 그룹3 갯수를 가져옴 (O)
         Map<String, Integer> groupInfo = groupCal.memberGroupInfo(n);
         int group4 = groupInfo.get("group4");
         int group3 = groupInfo.get("group3");
 
-        // 1차 차감
-        // todo 3. 인원을 그룹4, 그룹3에 차례대로 분배해줌
-        for(int i=0; i<group4; i++) {
-            List<Member> memberSet = new ArrayList<>();
-            memberSet.add(memberList.get(0));
-            memberSet.add(memberList.get(1));
-            memberSet.add(memberList.get(2));
-            memberSet.add(memberList.get(3));
-            memberList.remove(0);
-            memberList.remove(0);
-            memberList.remove(0);
-            memberList.remove(0);
-            group.put("group4_"+i, memberSet);
-        }
+        // n개의 그룹 생성
+        for(int i=0; i<group3 + group4; i++) {
+            List<Member> memberList = new ArrayList<>();
 
-        // 2차 차감
-        for(int i=0; i<group3; i++) {
-            List<Member> memberSet = new ArrayList<>();
-            memberSet.add(memberList.get(0));
-            memberSet.add(memberList.get(1));
-            memberSet.add(memberList.get(2));
-            memberList.remove(0);
-            memberList.remove(0);
-            memberList.remove(0);
-            group.put("group3_"+i, memberSet);
+            // 팀 전체
+            for(int j=0; j<teamInfo.size(); j++) {
+                String team = TeamEnum.valueOf(j).getTeam();
+                // member 카운트가 0이 아닌 경우만
+                if(teamInfo.get(team).size() > 0){
 
+                    // member 의 첫번 째 인원만 넣고 삭제해줌
+                    memberList.add(teamInfo.get(team).get(0));
+                    teamInfo.get(team).remove(0);
+
+                    // 멤버를 담는 리스트가 4일경우 다음 그룹으로 이동
+                    if (memberList.size() == 4) {
+                        break;
+                    }
+                }
+            }
+            group.put("group_" + i, memberList);
         }
 
         // todo 4. 모든 그룹정보에 그룹4, 그룹3을 담아서 리턴
-        System.out.println(group);
+        group.forEach((groupIndex,memberList)->{
+            System.out.println(groupIndex);
+            memberList.forEach(member->{
+                System.out.println(member.getMemberName());
+            });
+        });
 
-        // todo 5. 팀별로 중복이 없도록 처리하는 로직 추가
     }
 
 
